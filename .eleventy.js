a000f370a3ec26139ef2d13478671d6ee6449615
MrMacStripe <m.stuffer@mauricestuffer.com>
2021-09-06 15:27:55 +0200

    updated image generation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const path = require("path");
 const Image = require("@11ty/eleventy-img");
 const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
@@ -9,7 +10,7 @@ module.exports = function (eleventyConfig) {
 
 
   /* === START, add Assets to the bundle === */ 
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/meta");
   
   eleventyConfig.addWatchTarget('./src/tailwind.css')
   eleventyConfig.addWatchTarget('./src/main.js')
@@ -20,6 +21,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode('version', function () { return now })
   eleventyConfig.addNunjucksAsyncShortcode("picture", picture);
   eleventyConfig.addNunjucksAsyncShortcode("img", img);
+  eleventyConfig.addNunjucksAsyncShortcode("imgBg", imgBg);
   /* === END, add different image shortcodes to the bundle === */ 
 
   eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
@@ -73,12 +75,19 @@ module.exports = function (eleventyConfig) {
       // You bet we throw an error on missing alt (alt="" works okay)
       throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
     }
-  
+    
+    const folders = path.dirname(src).replace("src/assets/","")
     let metadata = await Image(src, {
-      widths: [160,320,640,960,1280,1600,1920,2560,null],
+      
+      widths: [160,600,768,120,null],
       formats: ['webp', 'jpeg'],
-      urlPath: "/assets/images/",
-      outputDir: "./_site/assets/images/",
+      urlPath: "/assets/"+folders,
+      outputDir: "./_site/assets/"+folders,
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src);
+        const name = path.basename(src, extension);    
+        return `${name}-${width}w.${format}`;
+      }
     });
   
     let lowsrc = metadata.jpeg[0];
@@ -96,7 +105,8 @@ module.exports = function (eleventyConfig) {
           alt="${alt}"
           loading="${loading}"
           title="${alt}"
-          decoding="async" />
+          decoding="async"
+        />
       </picture>`;
   }
 
@@ -106,12 +116,18 @@ module.exports = function (eleventyConfig) {
       // You bet we throw an error on missing alt (alt="" works okay)
       throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
     }
-  
+
+    const folders = path.dirname(src).replace("src/assets/","")
     let metadata = await Image(src, {
-      widths: [160,320,640,960,1280,1600,1920,2560,null],
+      widths: [160,600,768,120,null],
       formats: ["jpeg"],
-      urlPath: "/assets/images/",
-      outputDir: "./_site/assets/images/",
+      urlPath: "/assets/"+folders,
+      outputDir: "./_site/assets/"+folders,
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src);
+        const name = path.basename(src, extension);    
+        return `${name}-${width}w.${format}`;
+      }
     });
   
     let lowsrc = metadata.jpeg[0];
@@ -130,6 +146,47 @@ module.exports = function (eleventyConfig) {
         title="${alt}"
         decoding="async" />`;
   }
+
+
+/* generate responsive IMG BACKGROUND IMAGE ==================== */
+/* ================================================================ */
+async function imgBg(src, classes, alt, sizes = "100vw", loading="lazy") {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  const folders = path.dirname(src).replace("src/assets/","")
+  let metadata = await Image(src, {
+    widths: [640,1280,1920,2560,null],
+    formats: ["jpeg"],
+    urlPath: "/assets/"+folders,
+    outputDir: "./_site/assets/"+folders,
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);    
+      return `${name}-${width}w.${format}`;
+    }
+  });
+
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  return `<img
+    ${Object.values(metadata).map(imageFormat => {
+      return ` srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}" `;
+    }).join("\n")}        
+      class="${classes}"
+      src="${lowsrc.url}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
+      alt="${alt}"
+      loading="${loading}"
+      title="${alt}"
+      decoding="async" />`;
+}
+
+
 
 
   /* === START, 11ty export configuration === */
